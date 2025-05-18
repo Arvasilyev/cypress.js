943fab7f3a1bff4a0fa99124866d1f465b6a69a5
Arvasilyev <Artem.alex.11@yandex.ru>
2025-05-18 14:44:31 +0400

    Add files via upload

---
+++ cypress/e2e/Auto_QA_Login.qa.studio.cy.js
@@ -0,0 +1,86 @@
+describe('Тестирование формы логина и пароля', function(){
+it('1. Позитивный кейс авторизации', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Ввод Email
+    cy.get('#mail').type('user_login');
+    // Ввод Pass
+    cy.get('#pass').type('user_password');
+    // Нажатие на кнопку войти
+    cy.get('#loginButton').click();
+    // Проверка увторизации
+    cy.contains('Авторизация прошла успешно').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+it('2. Проверка логики восстановления пароля', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Нажать на кнопку "Забыли пароль"
+    cy.get('#forgotEmailButton').click();
+    // Ввод Email
+    cy.get('#mailForgot').type('user_login');
+    // Нажать энтер
+    cy.get('#mailForgot').type('{enter}');
+    // Проверка сообщения о восстановлении
+    cy.contains('Успешно отправили пароль на e-mail').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+it('3. Проверка негативного кейса авторизации(Не верный пароль)', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Ввод Email
+    cy.get('#mail').type('user_login');
+    // Ввод Pass
+    cy.get('#pass').type('user_password');
+    // Нажать энтер
+    cy.get('#pass').type('{enter}');
+    // Проверка сообщения о некоректных данных
+    cy.contains('Такого логина или пароля нет').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+it('4. Проверка негативного кейса авторизации(Не верный логин)', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Ввод Email
+    cy.get('#mail').type('user_login');
+    // Ввод Pass
+    cy.get('#pass').type('user_password');
+    // Нажать энтер
+    cy.get('#loginButton').click();
+    // Проверка сообщения о некоректных данных
+    cy.contains('Такого логина или пароля нет').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+it('5. Проверка негативного кейса авторизации(Без @ в Email)', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Ввод Email
+    cy.get('#mail').type('user_login');
+    // Ввод Pass
+    cy.get('#pass').type('user_password');
+    // Нажать энтер
+    cy.get('#loginButton').click();
+    // Проверка сообщения о некоректных данных
+    cy.contains('Нужно исправить проблему валидации').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+it('6. Приведение к строчным буквам поля Email', function() {
+    // Переход на главную страницу сайта
+    cy.visit('https://login.qa.studio');
+    // Ввод Email
+    cy.get('#mail').type('user_login');
+    // Ввод Pass
+    cy.get('#pass').type('user_password');
+    // Нажатие на кнопку войти
+    cy.get('#loginButton').click();
+    // Проверка увторизации
+    cy.contains('Авторизация прошла успешно').should('be.visible');
+    // Проверка наличия кнопки крестик
+    cy.get('#exitMessageButton > .exitIcon').should('exist');
+    })
+})
---
+++ cypress/e2e/Auto_QA_pokemonbattle.cy.js
@@ -0,0 +1,36 @@
+describe('Автотест на покупку Аватара', () => {
+  it('Покупку Аватара для тренера', () => {
+    // Переход на главную страницу сайта
+    cy.visit('https://pokemonbattle.ru/login');
+    // Ввод Email
+    cy.get('#k_email').type('user_login');
+    // Ввод Pass
+    cy.get('#k_password').type('user_password');
+    // Нажать энтер
+    cy.get('#k_password').type('{enter}');
+    // Вход в профиль
+    cy.get('.header_card_trainer').click();
+    // Вход в профиль
+    cy.get('.header_card_trainer').click();
+    // Смена Аратара
+    cy.get('.k_mobile > :nth-child(5)').click();
+    // Находим первую доступную кнопку в элементах с классом .available
+    cy.get('.available > button').first().click();
+    // вводим номер карты
+    cy.get('.card_number').type('4620869113632996');
+     // вводим CVV карты
+    cy.get('.card_csv').type('125');
+    // вводим срок действия карты
+    cy.get('.card_date').type('1226');
+    // вводим имя владельца
+    cy.get('.card_name').type('NAME');
+    // Нажимаем кнопку оплатить
+    cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+    //вводим код подтверждения СМС
+    cy.get('.threeds_number').type('56456');
+    // нажимаем кнопку Оплатить
+    cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();
+    // проверяем наличие и видимость сообщения об успешной покупке
+    cy.contains('Покупка прошла успешно').should('be.visible');
+  })
+})
